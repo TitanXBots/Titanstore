6fc60d2bc2d15cd1ee1fee11cf7a7aedb66210c0
Yash <titanmattersbot05@gmail.com>
2025-09-30 19:09:44 +0530

    Update user_id.py

--- plugins/user_id.py
+++ plugins/user_id.py
@@ -1,22 +1,20 @@
-# plugins/id.py
+#credit @codeflix_bots (telegram)
+
+"""Get id of the replied user
+Syntax: /id"""
+
 from pyrogram import filters, enums
 from pyrogram.types import Message
-from pyrogram import Client  # Import the Client class
-#from bot import Bot #No Need to import bot here since it get initialed during start
 
-@Client.on_message(filters.command("id") & filters.private)
-async def showid(client:Client, message: Message):  #Explicit typing with client : Client
+from bot import Bot
+
+
+@Bot.on_message(filters.command("id") & filters.private)
+async def showid(client, message):
     chat_type = message.chat.type
 
     if chat_type == enums.ChatType.PRIVATE:
         user_id = message.chat.id
         await message.reply_text(
-            f"Your User ID Is : {user_id}",
-            quote=True
+            f"<b>ʏᴏᴜʀ ᴜsᴇʀ ɪᴅ ɪs:</b> <code>{user_id}</code>", quote=True
         )
-
-# Jishu Developer
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
