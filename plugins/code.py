cc4fcbd35ef49edaf001f57eee21673592a0d9ee
Yash <titanmattersbot05@gmail.com>
2025-10-17 20:21:27 +0530

    Convert environment variable values to integers

--- plugins/code.py
+++ plugins/code.py
@@ -13,9 +13,9 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-F_SUB1 = os.environ.get('F_SUB1', '-2109163181')  # Read as strings first
-F_SUB2 = os.environ.get('F_SUB2', '-1917804203')
-F_SUB3 = os.environ.get('F_SUB3', '-1593340575')
+F_SUB1 = int(os.environ.get('F_SUB1', '-2109163181')) # Read as strings first
+F_SUB2 = int(os.environ.get('F_SUB2', '-1917804203'))
+F_SUB3 = int(os.environ.get('F_SUB3', '-1593340575'))
 
 ADMIN_USER_IDS = os.environ.get("ADMINS", "5356695781").split(",")
 
