bf3d500a210a64eb5ac1272c73738f778273b591
Yash <titanmattersbot05@gmail.com>
2025-11-04 19:36:02 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -9,7 +9,6 @@ from database.database import add_user, del_user, full_userbase, present_user
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
-
     if data == "help":
         await query.message.edit_text(
             text=HELP_TXT.format(first=query.from_user.first_name),
