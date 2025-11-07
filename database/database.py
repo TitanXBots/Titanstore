1e8ead31201605d5d4289372071feaf0216f0325
Yash <titanmattersbot05@gmail.com>
2025-11-07 18:16:20 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -13,6 +13,9 @@ database = dbclient[DB_NAME]
 
 user_data = database['users']
 
+admin_data = database['admins']
+
+
 
 
 async def present_user(user_id : int):
@@ -37,3 +40,34 @@ async def full_userbase():
 async def del_user(user_id: int):
     user_data.delete_one({'_id': user_id})
     return
+
+#---------New one -------------
+#------------------------------
+#TitanXBots
+
+
+async def is_admin(user_id: int):
+    """Check if user is admin"""
+    return admin_data.find_one({'_id': user_id}) is not None
+
+
+async def add_admin(user_id: int):
+    """Add user as admin"""
+    if not await is_admin(user_id):
+        admin_data.insert_one({'_id': user_id})
+        return True
+    return False
+
+
+async def remove_admin(user_id: int):
+    """Remove admin"""
+    admin_data.delete_one({'_id': user_id})
+    return True
+
+
+async def get_admins():
+    """Return all admin IDs"""
+    admins = []
+    async for doc in admin_data.find():
+        admins.append(doc['_id'])
+    return admins
