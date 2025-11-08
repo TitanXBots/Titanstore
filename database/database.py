c6d6b7088d34a50c4460a520ccaf9c03b0f34a35
Yash <titanmattersbot05@gmail.com>
2025-11-08 16:53:45 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,44 +1,53 @@
 #TitanXBots
 
-
-
-
 import pymongo, os
 from config import DB_URI, DB_NAME
-
 
 dbclient = pymongo.MongoClient(DB_URI)
 database = dbclient[DB_NAME]
 
-
 user_data = database['users']
+banned_users = database['banned_users']
 
-
-
-
-async def present_user(user_id : int):
+# -------------------------------
+# User management
+# -------------------------------
+async def present_user(user_id: int):
     found = user_data.find_one({'_id': user_id})
-    if found:
-        return True
-    else:
-        return False
+    return bool(found)
 
 async def add_user(user_id: int):
-    user_data.insert_one({'_id': user_id})
-    return
+    user_data.update_one({'_id': user_id}, {'$set': {'_id': user_id}}, upsert=True)
 
 async def full_userbase():
-    user_docs = user_data.find()
-    user_ids = []
-    for doc in user_docs:
-        user_ids.append(doc['_id'])
-        
-    return user_ids
+    users = user_data.find()
+    return [doc['_id'] for doc in users]
 
 async def del_user(user_id: int):
     user_data.delete_one({'_id': user_id})
-    return
 
-#---------New one -------------
-#------------------------------
+# -------------------------------
+# Ban system
+# -------------------------------
+async def ban_user(user_id: int, reason: str = "No reason provided"):
+    """Ban a user with an optional reason."""
+    banned_users.update_one(
+        {'_id': user_id},
+        {'$set': {'reason': reason}},
+        upsert=True
+    )
+
+async def unban_user(user_id: int):
+    """Unban a user."""
+    banned_users.delete_one({'_id': user_id})
+
+async def is_banned(user_id: int):
+    """Check if user is banned."""
+    return banned_users.find_one({'_id': user_id}) is not None
+
+async def get_ban_reason(user_id: int):
+    """Get ban reason."""
+    user = banned_users.find_one({'_id': user_id})
+    return user['reason'] if user else None
+
 #TitanXBots
