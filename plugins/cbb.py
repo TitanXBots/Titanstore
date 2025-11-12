66aa030d92f4808843de47da5bb8de913f3ab9b8
Yash <titanmattersbot05@gmail.com>
2025-11-12 20:11:16 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,7 +1,7 @@
 from pyrogram import Client
 from bot import Bot
 from config import *
-from Script import script
+from Script import COMMANDS_TXT, DISCLAIMER_TXT
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from database.database import add_user, del_user, full_userbase, present_user
 
