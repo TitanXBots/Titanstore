67f28ec0ee29af2ef2651ea09d216e030c65bcf2
Yash <titanmattersbot05@gmail.com>
2025-11-15 08:28:34 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -5,8 +5,10 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from bot import Bot
 from config import ADMINS
 from helper_func import encode, get_message_id
+from plugins.permissions import owner_only, admin_only
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
+@admin_only
 async def batch(client: Client, message: Message):
     while True:
         try:
@@ -41,6 +43,7 @@ async def batch(client: Client, message: Message):
 
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
+@admin_only
 async def link_generator(client: Client, message: Message):
     while True:
         try:
