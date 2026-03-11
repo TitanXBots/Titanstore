8059fb34456275e92e9b1220e8bf07c8bb324c43
Yash <titanmattersbot05@gmail.com>
2026-03-11 09:44:50 +0530

    Update Setting.py

--- plugins/Setting.py
+++ plugins/Setting.py
@@ -5,11 +5,9 @@ from config import OWNER_ID
 from database.database import is_admin
 
 
-###-------------------------------
-
-SETTINGS COMMAND
-
-###-------------------------------
+#-------------------------------
+#SETTINGS COMMAND
+#-------------------------------
 
 @Bot.on_message(filters.command("settings") & filters.private)
 async def settings_command(client: Bot, message: Message):
