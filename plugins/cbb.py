e5e57a58fd4362f45237d26d102b610636427bb5
Yash <titanmattersbot05@gmail.com>
2026-03-13 17:50:39 +0530

    Add files via upload

---
+++ plugins/cbb.py
@@ -0,0 +1,112 @@
+from pyrogram import Client
+from bot import Bot
+from config import *
+from Script import COMMANDS_TXT, DISCLAIMER_TXT
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from database.database import add_user, del_user, full_userbase, present_user
+
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+
+    if data == "help":
+        await query.message.edit_text(
+            text=HELP_TXT.format(first=query.from_user.first_name),
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("🧑‍💻 ᴄᴏɴᴛᴀᴄᴛ ᴏᴡɴᴇʀ", user_id=5356695781),
+                        InlineKeyboardButton("💬 ᴄᴏᴍᴍᴀɴᴅꜱ", callback_data="commands")
+                    ],
+                    [
+                        InlineKeyboardButton("⚓ ʜᴏᴍᴇ", callback_data="start"),
+                        InlineKeyboardButton("⚡ ᴄʟᴏꜱᴇ", callback_data="close")
+                    ]
+                ]
+            )
+        )
+
+    elif data == "about":
+        await query.message.edit_text(
+            text=ABOUT_TXT.format(first=query.from_user.first_name),
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("📜 ᴅɪꜱᴄʟᴀɪᴍᴇʀ", callback_data="disclaimer"),
+                        InlineKeyboardButton("🔐 ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ", url="https://github.com/TitanXBots/FileStore-Bot")
+                    ],
+                    [
+                        InlineKeyboardButton("⚓ ʜᴏᴍᴇ", callback_data="start"),
+                        InlineKeyboardButton("⚡ ᴄʟᴏꜱᴇ", callback_data="close")
+                    ]
+                ]
+            )
+        )
+
+    elif data == "start":
+        await query.message.edit_text(
+            text=START_MSG.format(first=query.from_user.first_name),
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("☆ ᴍᴏᴠɪᴇ ʀᴇQᴜᴇꜱᴛ ɢʀᴏᴜᴘ ☆", url="https://t.me/TitanMoviess")
+                    ],
+                    [
+                        InlineKeyboardButton("🧠 ʜᴇʟᴘ", callback_data="help"),
+                        InlineKeyboardButton("🔰 ᴀʙᴏᴜᴛ", callback_data="about")
+                    ],
+                    [
+                        InlineKeyboardButton("🤖 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ", url="https://t.me/TitanXBots"),
+                        InlineKeyboardButton("🔍 ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ", url="https://t.me/TitanMattersSupport")
+                    ],
+                    [
+                        InlineKeyboardButton("ʜᴏᴡ ᴛᴏ ᴜꜱᴇ ʙᴏᴛ", url="https://t.me/TitanXBackup/33")
+                    ]
+                ]
+            )
+        )
+
+    elif data == "commands":
+        await query.message.edit_text(
+            text=COMMANDS_TXT,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("🔙 ʙᴀᴄᴋ ᴛᴏ ʜᴇʟᴘ", callback_data="help")
+                    ],
+                    [
+                        InlineKeyboardButton("⚓ ʜᴏᴍᴇ", callback_data="start"),
+                        InlineKeyboardButton("⚡ ᴄʟᴏꜱᴇ", callback_data="close")
+                    ]
+                ]
+            )
+        )
+
+    elif data == "disclaimer":
+        await query.message.edit_text(
+            text=DISCLAIMER_TXT,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("🔰 ᴀʙᴏᴜᴛ", callback_data="about")
+                    ],
+                    [
+                        InlineKeyboardButton("⚓ ʜᴏᴍᴇ", callback_data="start"),
+                        InlineKeyboardButton("⚡ ᴄʟᴏꜱᴇ", callback_data="close")
+                    ]
+                ]
+            )
+        )
+
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
