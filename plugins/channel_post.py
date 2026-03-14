42ad07083666272bd0683773580aefe203979135
Yash <titanmattersbot05@gmail.com>
2026-03-15 00:57:08 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -1,41 +1,58 @@
-#TitanXBots
-
 import asyncio
 from pyrogram import filters, Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait
 
 from bot import Bot
-from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON
+from config import CHANNEL_ID, DISABLE_CHANNEL_BUTTON
 from helper_func import encode
+from database.database import is_admin  # <- dynamic admin check from DB
 
-@Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','users','broadcast','batch','genlink','stats','joinchannels','pypi','restart','settings','joinchannelon','joinchanneloff','admin','autodelete','autodeleteon','autodeleteoff','maintenance','ban','unban','bannedlist','addadmin','removeadmin','adminlist']))
+# -------------------------------
+# Forward private messages to DB channel and generate link (admin only)
+# -------------------------------
+@Bot.on_message(filters.private & ~filters.command([
+    'start','users','broadcast','batch','genlink','stats','joinchannels','pypi',
+    'restart','settings','joinchannelon','joinchanneloff','admin','autodelete',
+    'autodeleteon','autodeleteoff','maintenance','ban','unban','bannedlist',
+    'addadmin','removeadmin','adminlist'
+]))
 async def channel_post(client: Client, message: Message):
-    reply_text = await message.reply_text("Please Wait...!", quote = True)
+    user_id = message.from_user.id
+    if not await is_admin(user_id):
+        return  # ignore non-admins
+
+    reply_text = await message.reply_text("Please Wait...!", quote=True)
     try:
-        post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
+        post_message = await message.copy(chat_id=client.db_channel.id, disable_notification=True)
     except FloodWait as e:
         await asyncio.sleep(e.x)
-        post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
+        post_message = await message.copy(chat_id=client.db_channel.id, disable_notification=True)
     except Exception as e:
         print(e)
-        await reply_text.edit_text("Something went Wrong..!")
+        await reply_text.edit_text("Something went wrong..!")
         return
+
     converted_id = post_message.id * abs(client.db_channel.id)
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
 
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+    reply_markup = InlineKeyboardMarkup(
+        [[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]]
+    )
 
-    await reply_text.edit(f"<b>Here is your link</b>\n\n{link}", reply_markup=reply_markup, disable_web_page_preview = True)
+    await reply_text.edit(f"<b>Here is your link</b>\n\n{link}", reply_markup=reply_markup, disable_web_page_preview=True)
 
     if not DISABLE_CHANNEL_BUTTON:
         await post_message.edit_reply_markup(reply_markup)
 
+
+# -------------------------------
+# Edit channel posts to add share link automatically
+# -------------------------------
 @Bot.on_message(filters.channel & filters.incoming & filters.chat(CHANNEL_ID))
 async def new_post(client: Client, message: Message):
-
     if DISABLE_CHANNEL_BUTTON:
         return
 
@@ -43,7 +60,9 @@ async def new_post(client: Client, message: Message):
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+    reply_markup = InlineKeyboardMarkup(
+        [[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]]
+    )
     try:
         await message.edit_reply_markup(reply_markup)
     except Exception as e:
