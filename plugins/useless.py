178e8306eefb4f90e06025875af47942d62c6387
Yash <titanmattersbot05@gmail.com>
2026-03-15 00:40:40 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -1,19 +1,28 @@
 from bot import Bot
 from pyrogram.types import Message
 from pyrogram import filters
-from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT
+from config import BOT_STATS_TEXT, USER_REPLY_TEXT
 from datetime import datetime
-from helper_func import get_readable_time
+from helper_func import get_readable_time, is_admin  # <-- use database-based admin check
 
-@Bot.on_message(filters.command('stats') & filters.user(ADMINS))
+# -------------------------------
+# STATS COMMAND
+# -------------------------------
+@Bot.on_message(filters.command('stats') & filters.private)
 async def stats(bot: Bot, message: Message):
+    user_id = message.from_user.id
+    if not await is_admin(user_id):  # check if user is admin/owner from DB
+        return
+
     now = datetime.now()
     delta = now - bot.uptime
     time = get_readable_time(delta.seconds)
     await message.reply(BOT_STATS_TEXT.format(uptime=time))
 
-
+# -------------------------------
+# REPLY TO ALL USERS
+# -------------------------------
 @Bot.on_message(filters.private & filters.incoming)
-async def useless(_,message: Message):
+async def useless(_, message: Message):
     if USER_REPLY_TEXT:
         await message.reply(USER_REPLY_TEXT)
