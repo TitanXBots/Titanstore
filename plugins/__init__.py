a9a033581fc1e5368e5bbe63563883961c7a1a01
Yash <titanmattersbot05@gmail.com>
2026-07-02 17:51:30 +0530

    Clean up __init__.py and adjust import
    Removed comments and updated import statement.

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,14 +1,7 @@
-
-# TitanXBots
-# File Path: plugins/__init__.py
 from aiohttp import web
-from .route import routes  # Safe relative import from route.py
+from .route import routes
 
 async def web_server() -> web.Application:
-    """
-    Creates the web application context wrapper by pulling 
-    route blueprints from the separate route.py file.
-    """
     app = web.Application(client_max_size=30000000)
     app.add_routes(routes)
     return app
