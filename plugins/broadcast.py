7685ad0db52182da573b153677fa27783f940981
Yash <titanmattersbot05@gmail.com>
2026-07-27 23:36:35 +0530

    Fix import statement and add newline at EOF
    Corrected import statement formatting and added a newline at the end of the file.

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -1,4 +1,4 @@
-import asyncio
+Import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated, UserDeactivated
