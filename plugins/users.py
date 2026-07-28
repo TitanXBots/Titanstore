e416d2f988b17bdec962a49a9d92f674a767f017
Yash <titanmattersbot05@gmail.com>
2026-07-28 20:20:59 +0530

    Update users.py

--- plugins/users.py
+++ plugins/users.py
@@ -5,13 +5,13 @@ from database.database import is_admin, get_all_users
 @Client.on_message(filters.command("users") & filters.private)
 async def check_users_command(client: Client, message: Message):
     if not await is_admin(message.from_user.id):
-        return await message.reply_text("⚠️ Access Denied: Admins only!")
+        return await message.reply_text("⚠️ ᴀᴄᴄᴇꜱꜱ ᴅᴇɴɪᴇᴅ: ᴀᴅᴍɪɴꜱ ᴏɴʟʏ!")
         
-    loading_msg = await message.reply_text("⏳ Fetching user data...")
+    loading_msg = await message.reply_text("⏳ ꜰᴇᴛᴄʜɪɴɢ ᴜꜱᴇʀ ᴅᴀᴛᴀ...")
     users = await get_all_users()
     
     await loading_msg.edit_text(
-        f"📊 **Bot Statistics**\n\n"
-        f"👥 **Total Registered Users:** `{len(users)}`"
+        f"📊 ʙᴏᴛ ꜱᴛᴀᴛɪꜱᴛɪᴄꜱ\n\n"
+        f"👥 ᴛᴏᴛᴀʟ ʀᴇɢɪꜱᴛᴇʀᴇᴅ ᴜꜱᴇʀꜱ:`{len(users)}`"
     )
     
