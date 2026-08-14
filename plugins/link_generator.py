88519454b68ec076636a2f52d58cfdb4dff734a4
Yash <176878819+TitanXBots@users.noreply.github.com>
2026-08-14 21:10:22 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -24,11 +24,10 @@ async def link_generator(client: Client, message: Message):
         
         text = (
             "🎉 ʏᴏᴜʀ ꜱʜᴀʀᴇᴀʙʟᴇ ʟɪɴᴋ ʜᴀꜱ ʙᴇᴇɴ ɢᴇɴᴇʀᴀᴛᴇᴅ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ!\n\n"
-            f"<code>{link}</code>\n\n"
-            "🔗 ꜱʜᴀʀᴇ ʏᴏᴜʀ ʟɪɴᴋ ᴜꜱɪɴɢ ᴛʜᴇ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ.\n\n"
+            "🔗 ᴛᴏ ꜱʜᴀʀᴇ ʏᴏᴜʀ ʟɪɴᴋ, ꜱɪᴍᴘʟʏ ᴄʟɪᴄᴋ ᴛʜᴇ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ.\n\n"
             "ᴛʜᴀɴᴋ ʏᴏᴜ ꜰᴏʀ ʙᴇɪɴɢ ᴘᴀʀᴛ ᴏꜰ ᴛɪᴛᴀɴᴄɪɴᴇᴘʟᴇx! ᴡᴇ ᴛʀᴜʟʏ ᴀᴘᴘʀᴇᴄɪᴀᴛᴇ ʏᴏᴜʀ ꜱᴜᴘᴘᴏʀᴛ ᴀɴᴅ ʜᴏᴘᴇ ʏᴏᴜ ᴇɴᴊᴏʏ ᴜꜱɪɴɢ ᴏᴜʀ ʙᴏᴛ! 🚀"
         )
-        markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔄 Share URL ↗", url=f"https://t.me/share/url?url={link}")]])
+        markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔗 Open Link ↗", url=link)]])
         await message.reply_text(text, reply_markup=markup, disable_web_page_preview=True)
 
     elif cmd == "batch":
@@ -48,12 +47,11 @@ async def link_generator(client: Client, message: Message):
             
             text = (
                 "🎉 ʏᴏᴜʀ ʙᴀᴛᴄʜ ꜱʜᴀʀᴇᴀʙʟᴇ ʟɪɴᴋ ʜᴀꜱ ʙᴇᴇɴ ɢᴇɴᴇʀᴀᴛᴇᴅ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ!\n\n"
-                f"<code>{link}</code>\n\n"
-                "🔗 ꜱʜᴀʀᴇ ʏᴏᴜʀ ʟɪɴᴋ ᴜꜱɪɴɢ ᴛʜᴇ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ.\n\n"
+                "🔗 ᴛᴏ ꜱʜᴀʀᴇ ʏᴏᴜʀ ʟɪɴᴋ, ꜱɪᴍᴘʟʏ ᴄʟɪᴄᴋ ᴛʜᴇ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ.\n\n"
                 "ᴛʜᴀɴᴋ ʏᴏᴜ ꜰᴏʀ ʙᴇɪɴɢ ᴘᴀʀᴛ ᴏꜰ ᴛɪᴛᴀɴᴄɪɴᴇᴘʟᴇx! ᴡᴇ ᴛʀᴜʟʏ ᴀᴘᴘʀᴇᴄɪᴀᴛᴇ ʏᴏᴜʀ ꜱᴜᴘᴘᴏʀᴛ ᴀɴᴅ ʜᴏᴘᴇ ʏᴏᴜ ᴇɴᴊᴏʏ ᴜꜱɪɴɢ ᴏᴜʀ ʙᴏᴛ! 🚀"
             )
-            markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔄 Share URL ↗", url=f"https://t.me/share/url?url={link}")]])
+            markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔗 Open Link ↗", url=link)]])
             await message.reply_text(text, reply_markup=markup, disable_web_page_preview=True)
         except Exception as e:
             await message.reply_text(f"❌ <b>ᴇʀʀᴏʀ:</b> {e}")
-            
+        
